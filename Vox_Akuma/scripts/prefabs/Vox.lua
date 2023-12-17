8a1437bc729a56aac62a9d991b7d40b189193d69
VonLan233 <vonlan233@gmail.com>
2023-12-18 00:05:34 +0800

    2023.12.18.00:05 Main&Vox

---
+++ Vox_Akuma/scripts/prefabs/Vox.lua
@@ -0,0 +1,15 @@
+local MakePlayerCharacter = require "prefabs/player_common"
+local assets = {
+    Assets("ANIM", "anim/Vox.zip"),
+--     Assets("ANIM", "anim/KING_BIG_CHEST.zip"),
+}
+local prefabs={
+    "Voxsoul",
+}
+local start_inv={
+    "Haori",
+    "Onigiri",
+}
+
+-- local function HenShin()
+
